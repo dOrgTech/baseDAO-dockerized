bcc39c733d33a94b6f70313d1aa05a12abfc9552
namesty <nestor09amesty@gmail.com>
2021-04-04 21:40:51 +0200

    Enabled CORS

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Application as ExpressApp } from "express";
+import cors from "cors";
 import "dotenv/config";
 import { controllers } from "./controllers";
 
 const app: ExpressApp = express();
+app.use(cors());
+app.options('*', cors());
 app.use("/", controllers);
 
 export { app };
