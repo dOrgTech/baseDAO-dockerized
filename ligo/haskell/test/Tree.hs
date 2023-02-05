681b94e4789f4ca6adca1acc5f8f014bb0d2668d
Manank Patni <manank321@gmail.com>
2023-02-06 00:04:27 +0530

    Add FA1.2 token changes
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- ligo/haskell/test/Tree.hs
+++
@@ -1,4 +0,0 @@
--- SPDX-FileCopyrightText: 2021 Tezos Commons
--- SPDX-License-Identifier: LicenseRef-MIT-TC
-
-{-# OPTIONS_GHC -F -pgmF tasty-discover -optF --tree-display -optF --generated-module -optF Tree #-}
