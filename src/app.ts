8c3c4b466aeab5090e0136453b12d6bdca942aac
Ashutosh Kumar <ashutosh@w3dev.in>
2024-02-09 22:06:12 +0000

    Enabled Swagger API Docs

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.options('*', cors());
 
 // Set up Swagger UI at the root path
-// app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Add other controllers
 controllers.forEach((controller) => {
